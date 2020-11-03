823ba7cc69f2eaaf121bd705c5f014da9a98bca8
merrow-ragan <seragan3@gmail.com>
2020-11-03 15:34:06 -0500

    adds sign up, login, logout, index and show

---
+++ src/views/BoardsIndex.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="boards-index">
+ 
+  <div v-for="board in boards">
+    <h2>Title: {{ board.title }}</h2>
+    <p> Description: {{ board.description }}</p>
+    <!-- <p>Created by: {{ board.user_name }}</p> -->
+  </div>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      boards: []
+    };
+  },
+  created: function() {
+    this.indexBoards();
+  },
+  methods: {
+    indexBoards: function () {
+      axios.get("/api/boards").then((response) => {
+        console.log(response.data);
+        this.boards = response.data;
+      });
+    },
+  },
+};
+
+</script>
+
+
+
+
